7888031dbd8ac8bd47ab01e609969488ad8e248e
Drashti Gupta <105715992+drashti-a10@users.noreply.github.com>
2026-03-25 21:13:00 +0530

    Time: 5 ms (65.05%), Space: 56.5 MB (82.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,28 +4,29 @@
  */
 var isPalindrome = function(s) {
     let left = 0;
-    let right = s.length - 1;
-    while(left<right){
-        while(left<right && !alphaNumeric(s[left])){
-        left ++;
+    let right = s.length-1;
+    while(left < right){
+        while(left < right && !isAlphaNum(s[left])){
+            left++;
         }
-        while(left<right && !alphaNumeric(s[right])){
-        right--;
+        while(left < right && !isAlphaNum(s[right])){
+            right--;
         }
-        if(s[left].toLowerCase()!== s[right].toLowerCase()){
-        return false;
+        if(s[left].toLowerCase() !== s[right].toLowerCase()){
+            return false;
+
         }
         left++;
         right--;
-
     }
     return true;
 };
 
-function alphaNumeric(ch){
+function isAlphaNum(char){
     return(
-         (ch >= 'a' && ch <= 'z') ||
-        (ch >= 'A' && ch <= 'Z') ||
-        (ch >= '0' && ch <= '9')
+        (char >= 'a' && char <= 'z')||
+        (char >= 'A' && char <= 'Z') ||
+        (char >= '0' && char <= '9')
+
     );
 }
