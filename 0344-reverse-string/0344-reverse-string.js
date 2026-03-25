6e8a51bfd37a5b31f1bae39fa2b4625facf778d4
Drashti Gupta <105715992+drashti-a10@users.noreply.github.com>
2026-03-25 21:33:18 +0530

    Time: 4 ms (9.15%), Space: 62.5 MB (14.76%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -5,13 +5,10 @@
 var reverseString = function(s) {
     let left = 0;
     let right = s.length-1;
-    while(left<right){
-        let temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        left ++;
-        right --;
+    while(left < right){
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
     }
     return s;
-    
 };
