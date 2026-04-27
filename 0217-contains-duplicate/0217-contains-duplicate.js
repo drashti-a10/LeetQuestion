05f3febb2fc929564b307c6e3a2c823b93b856f2
Drashti Gupta <105715992+drashti-a10@users.noreply.github.com>
2026-04-27 10:36:28 +0530

    Time: 15 ms (79.37%), Space: 74.1 MB (51.37%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let set = new Set();
+    for(num of nums){
+        if(set.has(num)){
+            return true;
+        }
+        set.add(num);
+    }
+    return false;
+};
