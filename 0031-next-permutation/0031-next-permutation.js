78a3532de87b442187432fe9a064ee5e599fe0fd
Drashti Gupta <105715992+drashti-a10@users.noreply.github.com>
2026-05-04 13:36:42 +0530

    Time: 0 ms (100%), Space: 56.8 MB (36.71%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -3,30 +3,34 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-    let n = nums.length;
+    let n = nums.length, pivot = -1;
 
-    // STEP 1: find breakpoint
-    let i = n - 2;
-    while (i >= 0 && nums[i] >= nums[i + 1]) {
-        i--;
-    }
-
-    // STEP 2: swap
-    if (i >= 0) {
-        let j = n - 1;
-        while (nums[j] <= nums[i]) {
-            j--;
+    for(let i = n-2; i>=0; i--){
+        if(nums[i] < nums[i+1]){
+            pivot = i;
+            break;
         }
-        [nums[i], nums[j]] = [nums[j], nums[i]];
     }
 
-    // STEP 3: reverse
-    let left = i + 1;
-    let right = n - 1;
-
-    while (left < right) {
-        [nums[left], nums[right]] = [nums[right], nums[left]];
-        left++;
-        right--;
+    if(pivot === -1) {
+        reverse(nums, 0, n-1);
+        return;
     }
+
+    for(let i = n-1;i>pivot; i--){
+        if(nums[i] > nums[pivot]){
+            [nums[i], nums[pivot]] = [nums[pivot], nums[i]];
+            break;
+        }
+    }
+
+    reverse(nums, pivot + 1, n - 1);
 };
+
+function reverse(arr, start, end){
+    while(start < end){
+        [arr[start], arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;
+    }
+}
