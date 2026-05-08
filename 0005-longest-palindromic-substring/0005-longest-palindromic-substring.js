1aca639f5adc295857c22374ec26282982aab72c
Drashti Gupta <105715992+drashti-a10@users.noreply.github.com>
2026-05-08 14:51:32 +0530

    Time: 20 ms (63.16%), Space: 54.7 MB (81.86%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let maxLen=0;
+    let start =0;
+
+    function expand(left, right){
+        while(left >=0 && right<s.length && s[left] === s[right]){
+            if((right-left + 1)> maxLen){
+                maxLen = right - left + 1;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    for(let i =0; i<s.length; i++){
+        expand(i, i);
+        expand(i, i+1);
+    }
+    return s.substring(start, start + maxLen);
+};
