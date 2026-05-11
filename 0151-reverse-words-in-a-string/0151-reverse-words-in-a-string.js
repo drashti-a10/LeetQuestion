92077b6045e69093d7632bd33069862237aa9a8d
Drashti Gupta <105715992+drashti-a10@users.noreply.github.com>
2026-05-11 12:15:59 +0530

    Time: 1 ms (84.29%), Space: 57.1 MB (25.06%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,5 +3,28 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    return s.trim().split(/\s+/).reverse().join(" ");
+    s= s.split("").reverse().join("");
+
+    let ans= "";
+    let n = s.length;
+
+    for(let i = 0; i<n; i++){
+        let word = "";
+        while(i<n && s[i]!== ' '){
+            word += s[i];
+            i++;
+        }
+
+        word = word.split("").reverse().join("");
+
+        if(word.length > 0){
+            if(ans.length > 0){
+                ans += " ";
+            }
+
+            ans = ans + word;
+        }
+    }
+
+    return ans;
 };
