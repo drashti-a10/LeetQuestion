1bac32d92e15517051faa7eebfbbe8d349b7c7db
Drashti Gupta <105715992+drashti-a10@users.noreply.github.com>
2026-05-23 18:12:51 +0530

    Time: 0 ms (100%), Space: 55.5 MB (31.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = {};
+    let map = new Map();
     for(let i =0; i<nums.length; i++){
         let complement = target - nums[i];
-        if(map[complement] !== undefined){
-            return[map[complement], i]
+        if(map.has(complement)){
+            return[map.get(complement), i]
         }
-        map[nums[i]] = i;
+        map.set(nums[i], i);
     }
     return[];
 };
