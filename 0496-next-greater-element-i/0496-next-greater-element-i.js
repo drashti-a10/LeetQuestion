1bb3914003a07cb846a238487580d89704736db9
Drashti Gupta <105715992+drashti-a10@users.noreply.github.com>
2026-08-03 11:45:01 +0530

    Time: 4 ms (51.23%), Space: 55.6 MB (91.26%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+
+    let ans = [];
+
+    for(let i = 0; i < nums1.length; i++){
+
+        let index = nums2.indexOf(nums1[i]);
+
+        let greater = -1;
+
+        for(let j = index + 1; j < nums2.length; j++){
+
+            if(nums2[j] > nums1[i]){
+                greater = nums2[j];
+                break;
+            }
+
+        }
+
+        ans.push(greater);
+    }
+
+    return ans;
+};
