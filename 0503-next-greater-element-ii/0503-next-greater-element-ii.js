3092717512f4dfdcdaadebebd168caec2c5a7786
Drashti Gupta <105715992+drashti-a10@users.noreply.github.com>
2026-08-05 16:47:08 +0530

    Time: 7 ms (66.21%), Space: 61.1 MB (84.23%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElements = function(nums) {
+   
+    let nge = new Array(nums.length).fill(-1);
+    let stack = [];
+
+    for(let i = 2 * nums.length - 1; i>=0; i--){
+        while(stack.length>0 && stack[stack.length - 1] <= nums[i % nums.length]){
+            stack.pop();
+        }
+        if(i < nums.length){
+            nge[i] = stack.length === 0? -1:stack[stack.length - 1];
+        }
+
+        stack.push(nums[i % nums.length]);
+    }
+    return nge;
+};
