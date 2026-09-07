4c00dc5ff051ee9a4384e1b1f3fde680c3af0ce0
Drashti Gupta <105715992+drashti-a10@users.noreply.github.com>
2026-09-07 19:14:55 +0530

    Time: 0 ms (100%), Space: 54 MB (34.08%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    let N = n;
+    if(N < 0){
+        x = 1/x
+        N = -N;
+    }
+    let result = 1;
+    while(N>0){
+        if(N%2 === 1){
+            result = result * x
+        }
+        x= x*x;
+        N = Math.floor(N/2);
+    }
+    return result;
+};
